c3764d409a00639450c6589522b178257eaacfa9
marzie <marziemirjalili.78@gmail.com>
2023-09-21 14:11:53 +0200

    feat(home-page-hero): add hero section

---
+++ src/pages/home/components/hero/Hero.tsx
@@ -0,0 +1,48 @@
+import { FC } from "react";
+import { Container } from "../../../../components/Container";
+import styled from "styled-components";
+import { Flex } from "../../../../components/Flex";
+import { LeaderBoard } from "./components/leader-board/LeaderBoard";
+
+const StyledHeroContainer = styled.div`
+  background-image: url("./HeroImage.jpeg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 700px;
+  padding: 10px;
+`;
+
+const leaderBoardItems = [
+  {
+    id: 1,
+    playerPosition: "1",
+    playerName: "Tiger Woods",
+    playerscore: -9,
+  },
+  {
+    id: 2,
+    playerPosition: "2",
+    playerName: "Tiger Woods",
+    playerscore: -9,
+  },
+  {
+    id: 3,
+    playerPosition: "3",
+    playerName: "Tiger Woods",
+    playerscore: -9,
+  },
+];
+
+export const Hero: FC = () => {
+  return (
+    <StyledHeroContainer>
+      <Container>
+        <Flex justifyContent="center" alignItems="center">
+          <LeaderBoard leaderBoardItems={leaderBoardItems} />
+        </Flex>
+      </Container>
+    </StyledHeroContainer>
+  );
+};
