56726764a18b7d0b494e91abc09a9b3a73ab0a5b
marzie <marziemirjalili.78@gmail.com>
2023-09-21 14:17:48 +0200

    feat(home-page-hero): add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  cursor: pointer;
+  border-radius: 6px;
+  border: none;
+  font-style: normal;
+  font-weight: 400;
+  font-size: var(--fontSize-body-text);
+  line-height: 30px;
+  color: var(--color-text-primary);
+  background: var(--color-orange-primary);
+  padding: 10px 50px;
+`;
