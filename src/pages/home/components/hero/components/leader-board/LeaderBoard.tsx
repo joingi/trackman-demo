7aff6ec34ba083829519047b5e10f0507994295e
marzie <marziemirjalili.78@gmail.com>
2023-09-21 14:13:51 +0200

    feat(home-page-hero): add LeaderBoard component

---
+++ src/pages/home/components/hero/components/leader-board/LeaderBoard.tsx
@@ -0,0 +1,59 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { Flex } from "app/components/Flex";
+import BMWIcon from "app/assets/BMWIcon.svg";
+import {
+  LeaderBoardItem,
+  LeaderBoardItemPropsType,
+} from "./components/LeaderBoardItem";
+import { Button } from "app/components/Button";
+
+const LeaderBoardContainer = styled(Flex)`
+  background-color: var(--color-surface-secondary);
+  padding: 30px 25px;
+  height: 100%;
+  border-radius: 10px;
+  opacity: 0.9;
+`;
+const LeaderBoardTitle = styled.p`
+  font-size: var(--fontSize-header);
+  color: var(--color-text-primary);
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px;
+  letter-spacing: 0.75px;
+  margin-top: 10px;
+`;
+const LeaderBooardItemsContainer = styled(Flex)`
+  width: 100%;
+  margin: 25px 0;
+`;
+
+type LeaderBoardPropsType = {
+  leaderBoardItems: LeaderBoardItemPropsType[];
+};
+
+export const LeaderBoard: FC<LeaderBoardPropsType> = ({ leaderBoardItems }) => {
+  return (
+    <LeaderBoardContainer
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <img width="85px" height="85px" src={BMWIcon} alt="BMW" />
+      <LeaderBoardTitle>BMW Indoor Championship</LeaderBoardTitle>
+      <LeaderBooardItemsContainer flexDirection="column">
+        {leaderBoardItems.map((leaderBoardItem) => (
+          <LeaderBoardItem
+            key={leaderBoardItem.id}
+            id={leaderBoardItem.id}
+            playerPosition={leaderBoardItem.playerPosition}
+            playerName={leaderBoardItem.playerName}
+            playerscore={leaderBoardItem.playerscore}
+          />
+        ))}
+      </LeaderBooardItemsContainer>
+      <Button>Show More Courses</Button>
+    </LeaderBoardContainer>
+  );
+};
